7801de7b9568f9dcdc2579a2b52be997632bf127
XiongAmao <onycat@live.cn>
2017-08-15 17:20:16 +0800

    detach vuex

---
+++ src/vuex/mutations.js
@@ -0,0 +1,23 @@
+export default {
+    addTodo(state, payload) {
+        state.todoList.push(payload)
+    },
+    removeTodo(state, payload) {
+        let index = state.todoList.indexOf(payload)
+        state.todoList.splice(index, 1)
+    },
+    saveTodoList(state) {
+        let dataString = JSON.stringify(state.todoList)
+        window.localStorage.setItem('myTodos', dataString)
+    },
+    getLocalTodoList(state) {
+        let oldDataString = window.localStorage.getItem('myTodos')
+        let oldData = JSON.parse(oldDataString)
+        state.todoList = oldData || []
+    },
+    removeFinishedTodo(state) {
+        state.todoList = state.todoList.filter(function (elem) {
+            return !elem.done
+        })
+    }
+}
