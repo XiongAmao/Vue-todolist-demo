fcea69d431c0e1b3dbfc95bd6c0095faec049ff3
XiongAmao <onycat@live.cn>
2017-08-15 17:38:26 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -4,25 +4,30 @@ import Login from '@/components/Login'
 import TodoList from '@/components/TodoList'
 
 Vue.use(Router)
-const router = new Router({
-  routes: [
+const routes = [
     {
-      path: '/login',
-      name: 'Login',
-      component: Login,
-      alias: '/'
+        path: '/login',
+        name: 'Login',
+        component: Login,
+        alias: '/'
     }, {
-      path: '/todo',
-      name: 'TodoList',
-      component: TodoList
+        path: '/todo',
+        name: 'TodoList',
+        component: TodoList
     }
-  ],
+]
+const beforeEach = function(to,from,next){
+    next()
+    if (!to.name) next('Login')
+}
+const router = new Router({
+    routes
 })
 
 router.beforeEach((to, from, next) => {
-  next()
-  if(!to.name) next('Login')
-  // if router-name err , go to login
+    next()
+    if (!to.name) next('Login')
+    // if router-name err , go to login
 })
 
 export default router 
