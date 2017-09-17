ec12ec4dac888968ebb4735489c2355cfb51726e
XiongAmao <onycat@live.cn>
2017-09-17 10:19:59 +0800

    add README  introduction

---
+++ src/vuex/mutation-types.js
@@ -0,0 +1,5 @@
+export const TODO_ADD = 'TODO_ADD'
+export const TODO_REMOVE = 'TODO_REMOVE'
+export const TODOLIST_SAVE = 'TODOLIST_SAVE'
+export const LOCALSTORAGE_TODOLIST_GET = 'LOCALSTORAGE_TODOLIST_GET'
+export const LOGIN = 'LOGIN'
