bcccd7b815b198fdb5c7d559590c5310574ec016
XiongAmao <onycat@live.cn>
2017-10-04 20:45:47 +0800

    add favicon base64

--- src/main.js
+++ src/main.js
@@ -4,18 +4,11 @@ import router from './router/index'
 import store from './vuex/store'
 // import Vuex & Vue-router
 
-// import iView from 'iview'
-// import '../my-iview-theme/index.less'
-
 import 'normalize.css'
 import './assets/reset.css'
-// import './assets/icon-font/iconfont'
 
 import VueParticles from 'vue-particles'
 Vue.use(VueParticles)
-
-
-// Vue.use(iView)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
